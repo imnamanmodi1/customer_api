29cca728a03bb7637e45bd2719bef9fbeb260556
imnamanmodi1 <modi.naman14@gmail.com>
2020-02-24 21:20:39 +0530

    [DEBUG]

--- app.js
+++ app.js
@@ -28,7 +28,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// 2 lines
+app.use(express.static("./"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.use(
   "/dist/bundle",
