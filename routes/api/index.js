bb7e4bb3a8aaefb70d3768ae6aae495c3da95d53
imnamanmodi1 <modi.naman14@gmail.com>
2020-02-24 23:43:36 +0530

    [FIX] -  Fixed services bug where seperation can be now also done on basis of new line(\n)

--- routes/api/index.js
+++ routes/api/index.js
@@ -33,7 +33,7 @@ router.get("/customers/:email", function(req, res, next) {
     if (customerData) {
       var noData = "NA";
       var allServices = customerData.activeServices;
-      var arrServices = allServices.split(",");
+      var arrServices = allServices.split(",") || allServices.split("\n");
       console.log(arrServices, "test");
       var totalServices = arrServices.length;
       if (totalServices <= 1) {
